7d86454e0231187d842c13e43cbd78d9a24e5035
Ben Marshall <ben321mars@gmail.com>
2019-03-19 15:51:14 +0000

    Once again CMake has failed me, building the project manually, see if that works

--- Assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ben32/Desktop/assimp-4.0.1/assimp-4.0.1/contrib/zlib
-# Build directory: C:/Users/ben32/source/repos/BenMarshall98/Game-Engine/Assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
