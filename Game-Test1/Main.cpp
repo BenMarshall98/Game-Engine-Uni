5ef866e8088590050332935d4343d92efcf90978
Ben Marshall <ben321mars@gmail.com>
2019-04-28 15:45:50 +0100

    comments

--- Game-Test1/Main.cpp
+++ Game-Test1/Main.cpp
@@ -1,14 +1,18 @@
 #include "GLFWWindow.h"
 #include "SceneManager.h"
-#include "MenuScene.h"
+#include "MenuScene.h" //or "GameScene.h"
 
 int main(void)
 {
 	SceneManager * const sceneManager = SceneManager::Instance();
-	
+	//Optional: Used to set up the engine
 	sceneManager->ConfigEngine("EngineConfig.json");
+	
+	//The first scene in the game
 	sceneManager->NewScene(new MenuScene("GameChoosing.json"));
-	sceneManager->Run();
+	
+	//Starts the game engine running
+	sceneManager->Run(); 
 
 	delete sceneManager;
 
